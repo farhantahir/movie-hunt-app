5111bb4e286f8aeff5abc6d4018af333b42dd0a6
farhan <mf.tahir@outlook.com>
2019-03-24 15:38:57 +0500

    fix: cssModules issue with scss and external css

--- next.config.js
+++ next.config.js
@@ -1,9 +1,20 @@
-const withSass = require('@zeit/next-sass');
+const withPlugins = require('next-compose-plugins');
+const withSASS = require('@zeit/next-sass');
+const withCSS = require('@zeit/next-css');
 
-module.exports = withSass({
-  cssModules: true,
-  cssLoaderOptions: {
-    importLoaders: 1,
-    localIdentName: '[local]___[hash:base64:5]',
+const sass = [
+  withSASS,
+  {
+    cssModules: true,
+    cssLoaderOptions: {
+      importLoaders: 1,
+      localIdentName: '[local]___[hash:base64:5]',
+      camelCase: true,
+    },
   },
-});
+];
+
+const css = [withCSS, { cssModules: false }];
+const plugins = [sass, css];
+
+module.exports = withPlugins(plugins);
