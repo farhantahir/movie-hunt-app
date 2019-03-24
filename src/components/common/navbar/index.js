2f6c71ee80894ed299d94c42269748b14599962b
farhan <mf.tahir@outlook.com>
2019-03-24 16:12:02 +0500

    feat: added classnames pckg, updated scss

--- src/components/common/navbar/index.js
+++ src/components/common/navbar/index.js
@@ -1,22 +1,18 @@
 import React from 'react';
+import { node } from 'prop-types';
+import classnames from 'classnames/bind';
+import styles from './styles.scss';
 
-const navbar = () => (
+const cx = classnames.bind(styles);
+
+const navbar = ({ children }) => (
   <nav>
-    <div className="nav-wrapper">
-      <a className="brand-logo">Logo</a>
-      <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li>
-          <a href="sass.html">Sass</a>
-        </li>
-        <li>
-          <a href="badges.html">Components</a>
-        </li>
-        <li>
-          <a href="collapsible.html">JavaScript</a>
-        </li>
-      </ul>
-    </div>
+    <div className={cx('nav-wrapper', { navbarBg: true })}>{children}</div>
   </nav>
 );
+
+navbar.propTypes = {
+  children: node.isRequired,
+};
 
 export default navbar;
